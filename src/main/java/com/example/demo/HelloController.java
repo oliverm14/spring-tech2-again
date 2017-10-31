7dfb8e9b46068b5c5f6d6bd1c08bf7eb4f3587dd
alex-prog <coag@kea.dk>
2017-10-31 14:17:58 +0100

    get with param works now

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by coag on 31-10-2017.
@@ -12,11 +14,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
 
     @GetMapping("/")
-    public String sayHi(
+    public ModelAndView sayHi(
             @RequestParam(name = "name", defaultValue = "DAT16i")
             String name){
         System.out.println("name is "+name);
-        return "hi";
+        ModelAndView mv = new ModelAndView("hi");
+
+        mv.getModel().put("name", name);
+        return mv;
     }
 
 }
